ed2ae4ee5ada0d1a7a99473ff14f6c9188b7597a
Nikita <nik.nekit.14@mail.ru>
2022-09-22 10:16:50 +0500

    Создание структуры приложения, подключение библиотеки materialize, создание header,footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css'
+import Footer from './layout/Footer'
+import Header from './layout/Header'
+import Main from './layout/Main'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
---
+++ src/layout/Main.jsx
@@ -0,0 +1,10 @@
+
+function Main (){
+   return(
+      <main className="container content">
+         Hello React
+      </main>
+   )
+}
+
+export default Main
